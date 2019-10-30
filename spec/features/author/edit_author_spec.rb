55a402d8845b2a359092c40de13b713b4bf545b7
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-10-30 18:02:12 +0100

    FIX: Remove heading Create new user from details page.

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -3,32 +3,37 @@ require 'rails_helper'
 describe "Edit author page", type: :feature do
    before do
       @author = FactoryBot.create :author
-    end
- it "should render withour error" do
-   visit edit_author_path(@author)
- end
+   end
+   it "should render withour error" do
+      visit edit_author_path(@author)
+   end
 
- it "should change values of author" do
-   visit edit_author_path(@author)
-   page.fill_in 'author[first_name]', with: 'Alan'
-   page.fill_in 'author[last_name]', with: 'Turing'
-   page.fill_in 'author[homepage]', with: 'google.com'
-   find('input[type="submit"]').click
-   @author.reload
-   expect(@author.homepage).to eq('google.com')
- end
+   it "should change values of author" do
+      visit edit_author_path(@author)
+      page.fill_in 'author[first_name]', with: 'Alan'
+      page.fill_in 'author[last_name]', with: 'Turing'
+      page.fill_in 'author[homepage]', with: 'google.com'
+      find('input[type="submit"]').click
+      @author.reload
+      expect(@author.homepage).to eq('google.com')
+   end
 
- it "should show validation errors occured" do
-   visit edit_author_path(@author)
-   page.fill_in 'author[last_name]', with: ''
-   find('input[type="submit"]').click
-   expect(page).to have_text('error')
-end
+   it "should show validation errors occured" do
+      visit edit_author_path(@author)
+      page.fill_in 'author[last_name]', with: ''
+      find('input[type="submit"]').click
+      expect(page).to have_text('error')
+   end
 
-it "should explain errors" do
-   visit edit_author_path(@author)
-   page.fill_in 'author[last_name]', with: ''
-   find('input[type="submit"]').click
-  expect(page).to have_text('Last name can\'t be blank')
-  end
+   it "should explain errors" do
+      visit edit_author_path(@author)
+      page.fill_in 'author[last_name]', with: ''
+      find('input[type="submit"]').click
+      expect(page).to have_text('Last name can\'t be blank')
+   end
+
+   it "should not say create new author" do
+      visit edit_author_path(@author)
+      expect(page).not_to have_text("Create new author")
+   end
 end
