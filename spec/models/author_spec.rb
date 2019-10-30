d9aea60def86d29314a602f080766f69c03cb62f
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-10-30 10:05:12 +0100

    [3] Added test.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should show the full author name' do
+    author = Author.new('Alan', 'Turing', 'turing.com')
+    expect(author.first_name).to(eq('Alan'))
+    expect(author.last_name).to(eq('Turing'))
+    expect(author.homepage).to(eq('turing.com'))
+    expect(author.name).to(eq('Alan Turing'))
+  end
 end
