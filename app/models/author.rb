15fe099bebab74fee3251eea53a6cc992df6b6be
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-10-30 10:19:55 +0100

    [3] Added accessors for author.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+
+    def name
+            first_name.concat(" ", last_name)
+    end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it 'should show the full author name' do
-    author = Author.new('Alan', 'Turing', 'turing.com')
+    author = Author.new(first_name:'Alan', last_name:'Turing', homepage:'turing.com')
     expect(author.first_name).to(eq('Alan'))
     expect(author.last_name).to(eq('Turing'))
     expect(author.homepage).to(eq('turing.com'))
