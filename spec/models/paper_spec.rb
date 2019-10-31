b28d02a3130206f05873b82730bc747c134fbc65
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-10-31 11:19:53 +0100

    [14] Implemented m-to-n-relationship between authors and papers.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe Paper, type: :model do
   it "should not validate without title" do
     paper = Paper.new(venue:"Some venue", year:2002)
@@ -24,6 +23,12 @@ RSpec.describe Paper, type: :model do
   it "should only accept integer years" do
     paper = Paper.new(title:"Some title", venue:"Some venue", year:19.3)
     expect(paper).to_not be_valid
+  end
+
+  it "should have many authors" do
+    paper = Paper.new(title:"Some title", venue:"Some venue", year:2019)
+    paper.save
+    expect(paper.authors).to be_empty
   end
 
 
