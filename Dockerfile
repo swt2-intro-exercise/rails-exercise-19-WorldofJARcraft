89a93651f8e33883bfc204566c39dae4a1160be3
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-11-22 17:29:57 +0100

    Manual release at heroku, added change to document this.

--- Dockerfile
+++ Dockerfile
@@ -26,7 +26,7 @@ RUN ./bin/rails webpacker:install && \
     ./bin/rails generate react:install && \
     ./bin/rails webpacker:install:typescript
 
-
+#RUN ./bin/rails assets:precompile
 EXPOSE 3000
 ENV RAILS_SERVE_STATIC_FILES=true
 ENV RAILS_LOG_TO_STDOUT=true
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+id=$(docker inspect rails --format={{.Id}})
+curl -n -X PATCH https://api.heroku.com/apps/$APP/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "$id"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
